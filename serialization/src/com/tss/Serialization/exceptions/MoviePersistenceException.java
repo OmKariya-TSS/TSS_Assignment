2a94585576a2fb4e36b7a710c82fc2993277f9fc
Om Kariya <om.kariya@tssconsultancy.com>
2026-02-25 14:27:26 +0530

    final commit

--- serialization/src/com/tss/Serialization/exceptions/MoviePersistenceException.java
+++ serialization/src/com/tss/Serialization/exceptions/MoviePersistenceException.java
@@ -1,6 +1,7 @@
 package com.tss.Serialization.exceptions;
 
 public class MoviePersistenceException extends RuntimeException {
+    //in case of serialization failure , this exception is added
     public MoviePersistenceException(String message) {
         super(message);
     }
